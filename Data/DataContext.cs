627af736f9b16d6d413e9bfed56a63051ced2e76
Ben Folkens <bf853817@ohio.edu>
2021-10-13 10:23:15 -0400

    added employee model + data context

---
+++ Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using mis4200team2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mis4200team2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(): base("name=DefaultConnection") { }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable(nameof(Employee));
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mis4200team2.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        [Required, Display(Name="First Name"), StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required, Display(Name = "Last Name"), StringLength(50)]
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        [DataType(DataType.Date), DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode=true), Display(Name="Hire Date")]
+        public DateTime HireDate { get; set; }
+
+    }
+}
